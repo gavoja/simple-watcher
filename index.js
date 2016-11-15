e342498bb0809abd2fe1075a0cd766a03f561dd0
Michal Kochel <gavoja@gmail.com>
2016-11-15 10:19:49 +0100

    Reverted previous reporting behaviour as it turned out to be a better approach.

--- index.js
+++ index.js
@@ -3,18 +3,37 @@
 const fs = require('fs')
 const path = require('path')
 
+const TOLERANCE = 200
 const PLATFORMS = ['win32', 'darwin']
-const INTERVAL = 300
 
 // OS watcher.
-let watchFolder = (workingDir, recursive, callback) => {
+let watchFolder = (workingDir, recursive, tolerance, callback) => {
   let options = { persistent: true, recursive: recursive }
+  let last = { filePath: null, timestamp: 0 }
 
   let w = fs.watch(workingDir, options, (event, fileName) => {
     // On Windows fileName may actually be empty.
     // In such case assume this is the working dir change.
     let filePath = fileName ? path.join(workingDir, fileName) : workingDir
-    callback(filePath)
+
+    if (!tolerance) {
+      return callback(filePath)
+    }
+
+    fs.stat(filePath, (err, stat) => {
+      // If error, the file was likely deleted.
+      let timestamp = err ? 0 : (new Date(stat.mtime)).getTime()
+      let ready = err || timestamp - last.timestamp >= tolerance
+      let fileMatches = filePath === last.filePath
+      last.filePath = filePath
+      last.timestamp = timestamp
+
+      if (fileMatches && !ready) {
+        return
+      }
+
+      callback(filePath)
+    })
   })
 
   w.on('error', (e) => {
@@ -22,8 +41,7 @@ let watchFolder = (workingDir, recursive, callback) => {
   })
 }
 
-// Recursive fallback handler.
-let watchFolderFallback = (parent, callback) => {
+let watchFolderFallback = (parent, tolerance, callback) => {
   // This code is synchronous to be able to tell when it actually finishes.
   try {
     // Skip if not a directory.
@@ -31,29 +49,34 @@ let watchFolderFallback = (parent, callback) => {
       return
     }
 
-    watchFolder(parent, false, callback)
+    watchFolder(parent, false, tolerance, callback)
 
     // Iterate over list of children.
     fs.readdirSync(parent).forEach((child) => {
       child = path.resolve(parent, child)
-      watchFolderFallback(child, callback)
+      watchFolderFallback(child, tolerance, callback)
     })
   } catch (err) {
     console.error(err)
   }
 }
 
-let watch = (workingDir, callback) => {
+let watch = (workingDir, callback, tolerance) => {
   workingDir = path.resolve(workingDir)
+
+  // Set the default tolerance value.
+  tolerance = tolerance === undefined ? TOLERANCE : tolerance
+  // Enable tolerance only for Windows.
+  tolerance = process.platform === 'win32' ? tolerance : 0
 
   // Use recursive flag if natively available.
   if (PLATFORMS.indexOf(process.platform) !== -1) {
-    return watchFolder(workingDir, true, callback)
+    return watchFolder(workingDir, true, tolerance, callback)
   }
 
   // Attach handlers for each folder recursively.
   let cache = {}
-  watchFolderFallback(workingDir, (localPath) => {
+  watchFolderFallback(workingDir, tolerance, (localPath) => {
     fs.stat(localPath, (err, stat) => {
       // Delete cache entry.
       if (err) {
@@ -64,7 +87,7 @@ let watch = (workingDir, callback) => {
       // Add new handler for new directory and save in cache.
       if (stat.isDirectory() && !cache[localPath]) {
         cache[localPath] = true
-        watchFolder(localPath, false, callback)
+        watchFolder(localPath, false, tolerance, callback)
       }
     })
 
@@ -72,26 +95,10 @@ let watch = (workingDir, callback) => {
   })
 }
 
-let main = (workingDir, callback, interval) => {
-  interval = interval !== undefined ? interval : INTERVAL
-  // Enqueue items on change.
-  let queue = []
-  watch(workingDir, (filePath) => {
-    queue.push(filePath)
+if (require.main === module) {
+  watch(process.argv[2], (fileName) => {
+    console.log(`${fileName}`)
   })
-
-  // Start the interval.
-  setInterval(() => {
-    // Dequeue paths and store them as dictionary keys.
-    let dict = {}
-    while (queue.length) {
-      dict[queue.pop()] = true
-    }
-
-    // Run callback with unique paths.
-    let unique = Object.keys(dict)
-    unique.length && callback(unique)
-  }, interval)
 }
 
-module.exports = main
+module.exports = watch
--- test.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const watch = require('./index')
-
-watch(process.argv[2], (files) => {
-  console.log(files)
-})
